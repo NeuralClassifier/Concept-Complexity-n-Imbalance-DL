066061b6d221f449469ead836a39d6649c561696
Kushankur Ghosh <kush1999.kg@gmail.com>
2021-06-22 21:07:11 +0530

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import random
+from math import ceil
+import scipy
+import matplotlib.pyplot as plt
+from scipy import stats, optimize, interpolate
+import matplotlib.pyplot as plt
+import seaborn as sns
+from google.colab import files
+from scipy.stats import truncnorm
+import matplotlib.pyplot as plt
+from skimage import io
+from google.colab import files
+from PIL import Image, ImageDraw
+import random
+from matplotlib import cm
+import random
+
+uploaded = files.upload()
